2528826d65b8495cc2b25e9ed17aad5c826adb0c
shreyashdj <shreyashjadhav1799@gmail.com>
2022-07-11 18:52:55 +0530

    Added:Program to print Array elements on Odd position

---
+++ src/Array_Elements_on_Odd_Position.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Array_Elements_on_Odd_Position {
+
+	public static void main(String[] args) {		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the size of array");
+		int arraySize = sc.nextInt();
+		int array[] = new int[arraySize];
+		
+		System.out.println("Enter array elements");		
+		for (int i=0; i<arraySize; i++) {
+			array[i] = sc.nextInt();
+		}
+		System.out.println("Elements present on Odd Position are");
+		for (int i=1; i<arraySize; i=i+2) {		
+			System.out.print(array[i] +"  ");			
+		}	
+		
+		// TODO Auto-generated method stub
+sc.close();
+	}
+}
